bdf779c7cd408d367ada6724403908b4a26a34b7
Keshav Sharma <Keshav Sharma>
2025-07-21 12:56:27 +0530

    Green: Support custom delimiters with //[delimiter]\n format

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -4,7 +4,17 @@ export class StringCalculator {
       return 0;
     }
     
-    const nums = numbers.split(/[,\n]/);
+    let delimiter = /[,\n]/;
+    let numbersToProcess = numbers;
+    
+    if (numbers.startsWith('//')) {
+      const delimiterEnd = numbers.indexOf('\n');
+      const customDelimiter = numbers.substring(2, delimiterEnd);
+      delimiter = new RegExp(`[,\n${customDelimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}]`);
+      numbersToProcess = numbers.substring(delimiterEnd + 1);
+    }
+    
+    const nums = numbersToProcess.split(delimiter);
     return nums.reduce((sum, num) => sum + parseInt(num), 0);
   }
 }
