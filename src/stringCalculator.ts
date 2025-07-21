ff3c42c641a607def81ca6858c90b4fea95d15c7
Keshav Sharma <Keshav Sharma>
2025-07-21 12:55:31 +0530

    Green: Handle newlines between numbers using regex split

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -4,7 +4,7 @@ export class StringCalculator {
       return 0;
     }
     
-    const nums = numbers.split(',');
+    const nums = numbers.split(/[,\n]/);
     return nums.reduce((sum, num) => sum + parseInt(num), 0);
   }
 }
